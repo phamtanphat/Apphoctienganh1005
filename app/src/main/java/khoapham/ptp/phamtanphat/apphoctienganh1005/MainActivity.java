d04c78ea7638ab316dc19baf8162d83ded4540bd
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-06-21 14:23:09 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/apphoctienganh1005/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/apphoctienganh1005/MainActivity.java
@@ -6,11 +6,15 @@ import android.support.v7.widget.RecyclerView;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     EditText edtEn,edtVn;
     Button btnAddWord,btnCancel;
     RecyclerView recyclerViewWords;
+    ArrayList<Word> mangtuvung = new ArrayList<>();
+    WordAdapter wordAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +24,16 @@ public class MainActivity extends AppCompatActivity {
         btnAddWord = findViewById(R.id.buttonAddword);
         btnCancel = findViewById(R.id.buttonCancel);
         recyclerViewWords = findViewById(R.id.recyclerviewWords);
+
+        mangtuvung.add(new Word(0,"One","Một",true));
+        mangtuvung.add(new Word(1,"Two","Hai",false));
+        mangtuvung.add(new Word(2,"Three","Ba",false));
+        mangtuvung.add(new Word(3,"Four","Bốn",false));
+        mangtuvung.add(new Word(4,"Five","Năm",true));
+        mangtuvung.add(new Word(5,"Six","Sáu",true));
+
+        wordAdapter = new WordAdapter(mangtuvung);
+
+
     }
 }
